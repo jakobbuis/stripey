2126608a5257c927680a6e8080bf825935de7f57
Jakob Buis <jakob@jakobbuis.nl>
2019-12-15 11:07:34 +0100

    improve handling of locations outside of office

--- app/Calendar/Event.php
+++ app/Calendar/Event.php
@@ -40,14 +40,32 @@ class Event
     {
         $location = $this->googleEvent->location;
         if (!$location) {
+            return null; // unknown
+        }
+
+        // "Evoko" is a bluetooth-speaker listed as a room, ignore it
+        if (strpos($location, 'Evoko') !== false) {
             return null;
+        }
+
+        // Determine if the meeting is at the office, or somewhere else
+        $inOffice = false;
+        $needles = ['IN10', 'Kratonkade'];
+        foreach ($needles as $needle) {
+            if (strpos($location, $needle) !== false) {
+                $inOffice = true;
+                break;
+            }
+        }
+
+        // If outside of the office, use the original input unaltered
+        if (!$inOffice) {
+            return $location;
         }
 
         // Every meeting room starts with "IN10 | ", which we remove if applicable
         $matches = [];
         preg_match('/^IN10 \| (.*)$/', $location, $matches);
-        $room = $matches[1] ?? null;
-
-        return $room !== 'Evoko' ? $room : null;
+        return $matches[1] ?? null;
     }
 }
