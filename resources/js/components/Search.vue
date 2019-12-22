72d4e2588ea756ac1fb54debb2a0219cbedac896
Jakob Buis <jakob@jakobbuis.nl>
2019-12-22 23:44:30 +0100

    add label to search field

--- resources/js/components/Search.vue
+++ resources/js/components/Search.vue
@@ -1,10 +1,14 @@
 <template>
-    <input type="search"
-           name="search"
-           v-model="query"
-           placeholder="Find by name"
-           autofocus
-           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+    <div class="w-full">
+        <label for="search" class="sr-only">Search</label>
+        <input type="search"
+               name="search"
+               id="search"
+               v-model="query"
+               placeholder="Find by name"
+               autofocus
+               class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+    </div>
 </template>
 
 <script>
