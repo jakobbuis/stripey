ca11f74563b0d70125804a33f3d767aaef33df3c
Jakob Buis <jakob@jakobbuis.nl>
2019-12-22 11:40:55 +0100

    when determining location of an event, considers attending rooms first, and than fallback to the location given, while ignorning the Evoko

--- app/Calendar/Event.php
+++ app/Calendar/Event.php
@@ -39,33 +39,31 @@ class Event
     public function location(): ?string
     {
         $location = $this->googleEvent->location;
+
+        // If one or more rooms are attending, the meeting is there
+        $rooms = config('calendars.rooms');
+        $roomsTaken = array_filter($this->googleEvent->attendees, function ($attendee) use ($rooms) {
+            return isset($rooms[$attendee->email]);
+        });
+        if (count($roomsTaken) > 0) {
+            $roomNames = array_map(function ($room) use ($rooms) {
+                return $rooms[$room->email];
+            }, $roomsTaken);
+            sort($roomNames);
+            return implode(', ', $roomNames);
+        }
+
+        // Otherwise, check the location string
+        $location = $this->googleEvent->location;
         if (!$location) {
             return null; // unknown
         }
 
         // "Evoko" is a bluetooth-speaker listed as a room, ignore it
-        if (strpos($location, 'Evoko') !== false) {
+        if (strpos($location, 'IN10 | Evoko') !== false) {
             return null;
         }
 
-        // Determine if the meeting is at the office, or somewhere else
-        $inOffice = false;
-        $needles = ['IN10', 'Kratonkade'];
-        foreach ($needles as $needle) {
-            if (strpos($location, $needle) !== false) {
-                $inOffice = true;
-                break;
-            }
-        }
-
-        // If outside of the office, use the original input unaltered
-        if (!$inOffice) {
-            return $location;
-        }
-
-        // Every meeting room starts with "IN10 | ", which we remove if applicable
-        $matches = [];
-        preg_match('/^IN10 \| (.*)$/', $location, $matches);
-        return $matches[1] ?? null;
+       return $location;
     }
 }
--- config/calendars.php
+++ config/calendars.php
@@ -5,4 +5,15 @@ return [
      * The shared calendar that lists people as away
      */
     'afwezig' => 'in10.nl_h66qu9nplbml8nglbnu3tf4q48@group.calendar.google.com',
+
+    'rooms' => [
+        'in10.nl_38393137323438382d323330@resource.calendar.google.com' => 'Maaskamer',
+        'in10.nl_33373239363333352d313038@resource.calendar.google.com' => 'Podiumkamer',
+        'in10.nl_3332323536333034343933@resource.calendar.google.com' => 'Spreekkamer',
+        'in10.nl_2d36393534343830302d323430@resource.calendar.google.com' => 'Stand-up glas',
+        'in10.nl_2d33333732303539312d343233@resource.calendar.google.com' => 'Stand-up links',
+        'in10.nl_34383133323733312d363439@resource.calendar.google.com' => 'Stand-up rechts',
+        'in10.nl_38333138353730382d383635@resource.calendar.google.com' => 'Kamer Bjorn',
+        'in10.nl_3531343138353835353535@resource.calendar.google.com' => 'Kamer Arno/Xander',
+    ],
 ];
