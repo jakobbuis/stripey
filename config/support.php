5d8a356f27c799c5f94075f2d5b09c512b3f05fc
Jakob Buis <jakob.buis@in10.nl>
2019-12-26 00:25:49 +0100

    fix syntax error and obsolete comment in config/support.php

--- config/support.php
+++ config/support.php
@@ -3,7 +3,6 @@
 return [
     /*
      * The main menu contains a button for users to direct their support questions.
-     * This links directly into Slack, to the given team & user.
      */
-    'support_url' = 'slack://user?team=' . env('SLACK_TEAM_ID') . '&id=' . env('SLACK_CONVERSATION_ID'),
+    'support_url' => 'slack://user?team=' . env('SLACK_TEAM_ID') . '&id=' . env('SLACK_CONVERSATION_ID'),
 ];
